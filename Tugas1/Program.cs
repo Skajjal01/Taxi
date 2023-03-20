de072d63870cb3fc40f82b32ed067975d18aa7b5
skajjal01 <mahsa@students.amikom.ac.id>
2023-03-20 20:59:18 +0700

    Add project files.

---
+++ Tugas1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tugas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 5;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tugas1/Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas1
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drivername: {0}", DriverName);
+            Console.WriteLine("OnDuty: {0}", OnDuty ? "ya" : "tidak");
+            Console.WriteLine("NumPassenger: {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang perjalanan menjemput anda\n", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} telah mengantar anda\n", DriverName);
+        }
+    }
+}
